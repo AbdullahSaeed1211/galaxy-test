936ce450c955c9c0ad31dee4645e4d1f55efb106
Abdullah Saeed <85762594+AbdullahSaeed1211@users.noreply.github.com>
2025-02-22 22:42:29 +0530

    font match

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -2,7 +2,7 @@ export function HeroSection() {
   return (
     <section className="py-10 text-center max-w-6xl mx-auto">
       <div className="container px-4 md:px-6">
-        <h1 className="font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
+        <h1 className="font-bold tracking-tighter text-4xl md:text-6xl lg:text-7xl">
         AI Video-to-Video Transformation
         </h1>
         <p className="mx-auto mt-4 max-w-[700px] text-lg text-muted-foreground md:text-xl">
