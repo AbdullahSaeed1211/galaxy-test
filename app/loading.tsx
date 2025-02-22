0dc12bc9d47f6e1961d0fe8a7a35ac22a706082f
Abdullah Saeed <85762594+AbdullahSaeed1211@users.noreply.github.com>
2025-02-23 01:03:37 +0530

    added loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { LoadingSpinner } from './components/LoadingSpinner';
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <LoadingSpinner />
+      <span className="ml-2">Loading...</span>
+    </div>
+  );
+};
+
+export default Loading;
